0bb61649ae01667dd51130cc28e582556512a08d
dsad05 <dorota.sadowska95@gmail.com>
2025-02-12 23:32:38 +0100

    first commit

---
+++ MSTestSettings.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Playwright;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace PlaywrightTests
+{
+    [TestClass]
+    public class MSTestSettings
+    {
+        protected static IPlaywright _playwright;
+        protected static IBrowser _browser;
+        protected static IBrowserContext _context;
+
+        [AssemblyInitialize]
+        public static async Task AssemblyInitialize(TestContext context)
+        {
+            _playwright = await Playwright.CreateAsync();
+            _browser = await _playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = false, 
+                SlowMo = 100
+            });
+
+            _context = await _browser.NewContextAsync();
+        }
+
+        [AssemblyCleanup]
+        public static async Task AssemblyCleanup()
+        {
+            await _browser.CloseAsync();
+            _playwright.Dispose();
+        }
+    }
+}
---
+++ PlaywrightTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Playwright;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace PlaywrightTests
+{
+    [TestClass]
+    public class PlaywrightTest
+    {
+        private static IPlaywright _playwright;
+        private static IBrowser _browser;
+
+        [ClassInitialize]
+        public static async Task Setup(TestContext context)
+        {
+            _playwright = await Playwright.CreateAsync();
+            _browser = await _playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions 
+            { 
+                Headless = false
+            });
+        }
+
+        [TestMethod]
+        public async Task TestGoogleSearch()
+        {
+            var page = await _browser.NewPageAsync();
+            await page.GotoAsync("https://www.google.com");
+            var title = await page.TitleAsync();
+            Assert.IsTrue(title.Contains("Google"));
+        }
+
+        [ClassCleanup]
+        public static async Task Cleanup()
+        {
+            await _browser.CloseAsync();
+            _playwright.Dispose();
+        }
+    }
+}

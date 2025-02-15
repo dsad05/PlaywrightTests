118afa94de41b0ca0ec5dded6d849841cacc0a90
dsad05 <dorota.sadowska95@gmail.com>
2025-02-15 20:31:14 +0100

    new: add CRUD Tests

---
+++ Tests/CRUDTests.cs
@@ -0,0 +1,58 @@
+using Microsoft.Playwright.MSTest;
+using Microsoft.Playwright;
+using TODO_MVC_Pages;
+using TODO_MVC.Helpers;
+
+namespace TODO_MVC.Tests.UnitTests
+{
+    [TestClass]
+    public class AddTodoUnit : PageTest
+    {
+        private TodoPage? _todoPage;
+        private TodoPage TodoPage => _todoPage ?? throw new InvalidOperationException("TestSetup() was not run!");
+
+        [TestInitialize]
+        public async Task TestSetup()
+        {
+            _todoPage = new TodoPage(Page);
+            await _todoPage.InitializePage();
+            Assert.IsTrue(await TodoPage.IsTodoListEmpty());
+        }
+
+        [TestMethod]
+        public async Task Should_Add_Valid_Todo()
+        {
+            string todoName = TestDataGenerator.GenerateUniqueTodoName();
+            await TodoPage.AddTodo(todoName);
+            Assert.IsTrue(await TodoPage.IsTodoVisible(todoName));
+        }
+
+        [TestMethod]
+        public async Task Should_Complete_Todo()
+        {
+            string todoName = TestDataGenerator.GenerateUniqueTodoName();
+            await TodoPage.AddTodo(todoName);
+            await TodoPage.CompleteTodo(todoName);
+            Assert.IsTrue(await TodoPage.IsTodoCompleted(todoName));
+        }
+
+        [TestMethod]
+        public async Task Should_Clear_Completed_Todo() 
+        {
+            string todoName = TestDataGenerator.GenerateUniqueTodoName();
+            await TodoPage.AddTodo(todoName);
+            await TodoPage.CompleteTodo(todoName);
+            Assert.IsTrue(await TodoPage.IsTodoCompleted(todoName));
+            await TodoPage.ClearCompleted();
+            Assert.IsTrue(await TodoPage.IsTodoListEmpty());
+        }
+
+        [TestMethod]
+        public async Task Should_Delete_Todo(){
+            string todoName = TestDataGenerator.GenerateUniqueTodoName();
+            await TodoPage.AddTodo(todoName);
+            await TodoPage.DeleteTodo(todoName);
+            Assert.IsTrue(await TodoPage.IsTodoListEmpty());            
+        }
+    }
+}
